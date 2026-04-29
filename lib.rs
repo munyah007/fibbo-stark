6f81378cfe6244d397b2e108970367a815b245cf
munyah007 <hlomani008/2gmail.com>
2026-04-29 15:13:41 -0400

    to all verification

--- lib.rs
+++ lib.rs
@@ -32,6 +32,10 @@ mod tests {
     use num_bigint::BigUint ;
     use num_traits::cast::ToPrimitive ;
     use rs_merkle::{MerkleProof , algorithms::Sha256} ;
+    const MOD: i64 = 97 ;
+    const MOD_2: i64 = 97 ;
+
+    
 
  
   
@@ -214,6 +218,7 @@ struct Proof {
  let fx: i64 = values[index].clone();
  let fx_p1: i64 = values[pindex_p1].clone() ;
  let fx_p2: i64 = values[pindex_p2].clone() ;
+ let cx_cl: Vec<i64> = values5.clone() ;
 let proof: Proof = Proof{
                    proof_fx: merk::proof_prod(&values , index),
                    proof_fx_p1: merk::proof_prod(&values , pindex_p1) ,
@@ -242,6 +247,194 @@ let proof: Proof = Proof{
                    shift: shift_par ,
                    fib_targ: fib_targ, 
                    orig_k: k };
+
+fn reduce_layer(vals:  &Vec<i64>) -> Vec<i64> {
+let order: usize = vals.len() ;
+let half: usize = (order / 2)   ;
+let half_ = half.saturating_sub(1);
+let indeces: Vec<usize> = (0..=half_).collect();
+let mut container: Vec<i64> = Vec::new() ;
+ let xs: Vec<Omega> = basic::prod_sub_gr((order as u32) , 0) ;
+  let xs_proc: Vec<Omega> = (xs.iter()).map(move |x| x.process_bigger_than_kov2(order as u32)).collect();
+   let root: [u8 ; 32 ] = merk::root_prod(&vals);
+   let alpha_big = BigUint::from_bytes_le(&root);
+    
+
+   let  p_div  = BigUint::from(MOD_2 as u32 );
+
+   let alpha = (&alpha_big % &p_div) ;
+   let alpha_ = ToPrimitive::to_i64(&alpha).unwrap(); 
+for x in indeces {
+    let x_at_ind = poly2::evaluate_Omega(&xs_proc[x] ,order as i64);
+    let x_ = (x + half );
+    let con = 2 ;
+    let term1_num = poly2::mod_add_2(&vals[x] , &vals[x_]) ;
+    let term1_den = poly2::mod_inv(2);
+    let term2_num = poly2::mod_sub_2(&vals[x] ,&vals[x_]) ;
+    let term2_den_pen = poly2::mod_mul_2(&con ,&x_at_ind) ;
+    let term2_den = poly2::mod_inv(term2_den_pen);
+    let term1 = poly2::mod_mul_2(&term1_num ,&term1_den);
+    let term2 = poly2::mod_mul_2(&term2_num ,&term2_den);
+    let term2_ = poly2::mod_mul_2(&term2 , &alpha_); 
+    let term = poly2::mod_add_2(&term1 , &term2_ );
+    container.push(term) ;
+}
+container 
+}
+
+struct LayerProof {
+     index: usize ,
+     root_vals: [u8 ; 32 ],// root of real trace 
+     root_nl: [u8 ; 32 ] , // root1
+     proof_fx: MerkleProof<Sha256>,
+     proof_f_min_x: MerkleProof<Sha256>,
+     proof_nl: MerkleProof<Sha256> ,
+     fx: i64 ,
+     f_min_x: i64 ,
+     fx_nl: i64 ,
+     length: usize ,
+
+ }
+
+ fn layer_a_to_b_proof (vals: &Vec<i64> ) -> LayerProof{
+   let root: [u8 ; 32 ] = merk::root_prod(&vals);
+
+   let index_big = BigUint::from_bytes_le(&root);
+   let for_np = (vals.len() as i64 / 2 ) as usize ;// index should come from the lower half 
+   let half = for_np.clone() ;
+   let np = BigUint::from(for_np as u32);
+   let index = (&index_big % &np).to_usize().expect("Index too large")   ;
+   let next_layer: Vec<i64> = reduce_layer(&vals ) ;
+   println!("New layer {:?}" ,next_layer);
+   let fx = vals[index].clone() ;
+   let f_min_x = vals[index + for_np] ;
+   
+   let red_val = next_layer[index] ;// the value of interest will be at the same index in its layer 
+   let next_layer_root = merk::root_prod(&next_layer);
+   
+     LayerProof{
+         length: vals.len() ,
+         index: index , // has to be verified that it came from the root 
+         root_vals: root ,
+         root_nl: next_layer_root ,
+         proof_fx:   merk::proof_prod(&vals , index)  ,
+         proof_f_min_x:   merk::proof_prod(&vals , index + for_np)  ,
+         proof_nl: merk::proof_prod(&next_layer , index)  , // used to prove that the commited value is indeed in the root 
+         fx: fx ,
+         f_min_x: f_min_x ,
+         fx_nl: red_val ,
+
+     }
+
+
+ }
+
+let layer2: Vec<i64> = reduce_layer(&cx_cl) ;
+println!("Layer 2 {:?}" , layer2) ;
+
+fn produce_comp_proofs(input: &Vec<i64>) -> Vec<LayerProof> {
+let mut complete_proofs : Vec<LayerProof> = Vec::new() ;
+let mut curr_input: Vec<i64> = reduce_layer(input) ;
+complete_proofs.push(layer_a_to_b_proof(input));
+let mut counter: usize = 2 ;
+while curr_input.len() != 1  {
+    complete_proofs.push(layer_a_to_b_proof(&curr_input)) ;
+    curr_input = reduce_layer(&curr_input) ;
+    println!(" Round {} done" , counter) ;
+    counter += 1 ;
+ }
+println!("All proofs produced") ;
+complete_proofs
+
+}
+
+let mut  comp_proof: Vec<LayerProof> = produce_comp_proofs(&cx_cl) ;
+let max_index: usize  = produce_comp_proofs(&cx_cl).len() - 1 ;
+
+fn verify_individual_proof(layer_proof: LayerProof ) -> bool {
+//verify index came from the root 
+ let root: [u8 ; 32 ] = layer_proof.root_vals;
+ let root_1: [u8 ; 32 ] = root.clone() ;
+   let index_big = BigUint::from_bytes_le(&root);
+   let for_np = (layer_proof.length as i64 / 2 ) as usize ;// index should come from the lower half 
+   let half = for_np.clone() ;
+   let np = BigUint::from(for_np as u32);
+   let index = (&index_big % &np).to_usize().expect("Index too large")   ;
+   assert_eq!( index , layer_proof.index) ;
+   println!("Index verified , that it came from root !") ;
+  let fx_ver = merk::verify_proof(layer_proof.proof_fx ,layer_proof.root_vals, layer_proof.fx , layer_proof.index ,layer_proof.length) ;
+    assert!(fx_ver);
+    println!("fx verified to be at index in the root ") ;
+ let f_minx_ver = merk::verify_proof(layer_proof.proof_f_min_x ,root_1 , layer_proof.f_min_x , layer_proof.index + half  ,layer_proof.length) ;
+    assert!(f_minx_ver);
+    println!("f_minx verified to be at index in the root ") ; 
+ let f_x_nl_ver = merk::verify_proof(layer_proof.proof_nl ,layer_proof.root_nl, layer_proof.fx_nl , layer_proof.index  ,half) ;
+    assert!(f_x_nl_ver);
+    println!("f_x_nl_ver  verified to be at index in the root ") ;
+
+    // calculating relevant x for verification in calculation
+   let eval_xs: Vec<Omega> = basic::prod_sub_gr(layer_proof.length  as u32, 0) ;
+  
+
+  let orig_proc_out: Omega = eval_xs[index].process_bigger_than_kov2(layer_proof.length as u32 );
+  
+  let x_at_index = poly2::evaluate_Omega(&orig_proc_out ,layer_proof.length as i64) ;
+  println!("x successfully calculated !!") ;
+
+  // performing the calculation
+  //
+ let alpha_big = BigUint::from_bytes_le(&root_1);
+    
+
+   let  p_div  = BigUint::from(MOD_2 as u32 );
+
+   let alpha = (&alpha_big % &p_div) ;
+   let alpha_ = ToPrimitive::to_i64(&alpha).unwrap(); 
+  let con = 2 ;
+    let term1_num = poly2::mod_add_2(&layer_proof.fx , &layer_proof.f_min_x) ;
+    let term1_den = poly2::mod_inv(2);
+    let term2_num = poly2::mod_sub_2(&layer_proof.fx ,&layer_proof.f_min_x) ;
+    let term2_den_pen = poly2::mod_mul_2(&con ,&x_at_index) ;
+    let term2_den = poly2::mod_inv(term2_den_pen);
+    let term1 = poly2::mod_mul_2(&term1_num ,&term1_den);
+    let term2 = poly2::mod_mul_2(&term2_num ,&term2_den);
+    let term2_ = poly2::mod_mul_2(&term2 , &alpha_); 
+    let term = poly2::mod_add_2(&term1 , &term2_ );
+    assert_eq!(term , layer_proof.fx_nl) ;
+    println!("Reduction was valid!!") ;
+   true 
+}
+
+
+fn verify_all_layer_proofs(mut lay_proofs:  Vec<LayerProof>, max_index: usize)-> bool {
+let vals: Vec<i64> = vec![0,0] ; // dummy values for initialisation 
+let mut  root_next_for_last: [u8 ; 32 ] = merk::root_prod(&vals);
+for x in (0..= max_index) {
+    let proof = lay_proofs.remove(0) ;
+    if x == 0 {
+        root_next_for_last = proof.root_nl.clone() 
+         }
+    else  {
+        // asserting that what was produced as next layer and hence commited in the last layer is
+        // what is being used at the vals for this layer thus ensuring linkage of the provided
+        // layers .That the values used comes from those layers is what is accomplished by
+        // verify_individual_proof .
+        assert_eq!(root_next_for_last , proof.root_vals) ;
+        root_next_for_last  = proof.root_nl.clone() ;
+    }
+    let verdict  = verify_individual_proof(proof) ;
+    
+    assert!(verdict) ;
+    println!("We are at {}" , x) ;
+}
+println!("all proofs individually verified and linkage established .") ;
+
+true 
+}
+
+let allproofs: bool = verify_all_layer_proofs(comp_proof, max_index) ;
+println!("All layers proofs :{}", allproofs) ;
+
   fn verify (input: Proof) ->() {
   let eval_xs: Vec<Omega> = basic::prod_sub_gr((input.eval_order as u32) , 0) ;
   
@@ -412,7 +605,7 @@ let proof: Proof = Proof{
 
   }
 verify(proof) ;
-     
+ 
 
     }
 }
